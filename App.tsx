649fe225a3b2b93583d4fd16bf9d6df33806d2d0
ericksjp <ericksjp703.gmail.com>
2026-01-11 20:07:46 -0300

    feat(app): ativar gerenciamento global de turmas

--- App.tsx
+++ App.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "./src/contexts/AuthContext";
+import { ClassProvider } from "./src/contexts/ClassContext";
 import { AppNavigator } from "./src/navigation/AppNavigator";
 import { SnackbarProvider } from "./src/contexts/SnackBarContext";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <AuthProvider>
-        <SnackbarProvider>
-          <AppNavigator />
-          <StatusBar style="auto" />
-        </SnackbarProvider>
-      </AuthProvider>
-    </SafeAreaProvider>
-
-  );
+    return (
+        <SafeAreaProvider>
+            <AuthProvider>
+                <ClassProvider>
+                    <SnackbarProvider>
+                        <AppNavigator />
+                        <StatusBar style="auto" />
+                    </SnackbarProvider>
+                </ClassProvider>
+            </AuthProvider>
+        </SafeAreaProvider>
+    );
 }
